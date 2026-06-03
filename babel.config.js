1e8e9536d9b25dd9499910c2821b99c89c5c76e1
vaibhav23283 <vaibhavhongal63@gmail.com>
2026-06-03 18:06:21 +0530

    fix: remove duplicate react-native-worklets plugin from babel config

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function (api) {
     return {
         presets: ['babel-preset-expo'],
         plugins: [
-            'react-native-worklets/plugin',
             'react-native-reanimated/plugin',
         ],
     };
